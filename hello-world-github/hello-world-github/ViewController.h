d9f4c460ed90d23e569c5162e52cc64042903e39
happyxiechao <hh.xiechao@gmail.com>
2015-06-24 16:48:09 +0800

    a test commit for github

---
+++ hello-world-github/hello-world-github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello-world-github
+//
+//  Created by 谢超 on 15/6/24.
+//  Copyright (c) 2015年 XC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hello-world-github/hello-world-github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hello-world-github
+//
+//  Created by 谢超 on 15/6/24.
+//  Copyright (c) 2015年 XC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
